000a4418746a27c569d63ab6ea5cade0204c68bc
Obinna Nnodim <obiknows88@gmail.com>
2019-07-09 10:23:01 -0700

    added in Instagram plugin for Instagram story wall

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    // For Instagram
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `oursoulagenda`,
+      },
+    },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
